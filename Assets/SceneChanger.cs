ab7e89f103d22be6eed4e21b2ceebd7255ba7f1a
ERNST\mati <mati10005@gmail.com>
2022-05-10 14:36:50 +0300

    open project

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+	public void ChangeScene(string sceneName)
+	{
+		Debug.Log("Loading scne " + sceneName);
+		//SceneManager.LoadScene(name);
+		SceneManager.LoadScene(int.Parse(sceneName));
+	}
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
